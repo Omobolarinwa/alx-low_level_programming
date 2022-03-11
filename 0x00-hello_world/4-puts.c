1e3e58c15e6c867aad0a61e4c40cf8a4583a6d55
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-03-11 04:31:32 +0100

    updated puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,11 @@
  *
  * Description - prints "Programming is like building a multilingual puzzle"
  *
- * return value: 0, success
+ * Return value: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\"");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
