c5414bd023e553befe1514fc80dbdc75532f05fa
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-03-11 21:18:38 +0100

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints the single numbers in base 10
+ *
+ * Return: returns 0
+ **/
+
+int main(void)
+{
+for (int i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+}
