a6d05b12bdcbec2a75b7211dfeca2bc4a47cf63d
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-03-11 05:21:34 +0100

    updated 5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,10 @@
  * Description: prints "with proper grammar, but the outcome is a piece of
  * art,"
  *
- * Returns 0
- */
+ * Return: returns 0
+ **/
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+     printf("with proper grammar, but the outcome is a piece of art,\n");
+     return (0);
 }
