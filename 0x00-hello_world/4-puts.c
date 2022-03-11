e7b6e984234a09d4dd89168fe270e217db9c565a
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-03-11 04:49:01 +0100

    updated 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - entry point
  *
- * Description - prints "Programming is like building a multilingual puzzle"
- * with new line
+ * Description - prints "Programming is like building a multilingual puzzle"with a newline.
  *
  * Return value: 0
- */
+ **/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-    return(0);
-}
+    return (0);}
