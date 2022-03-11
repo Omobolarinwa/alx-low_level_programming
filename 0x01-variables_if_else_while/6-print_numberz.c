1d9268e515e3da7f2368bf80e23da29cc8d87329
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-03-11 22:10:08 +0100

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints the single numbers in base 10
+ *
+ * Return: returns 0
+ **/
+
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+printf("%d", num);
+num++;
+}
+printf("\n");
+return (0);
+}
