6b8462c992a828283c0de0ce66b22f2775bfccdb
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-03-11 04:20:28 +0100

    updated puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
  * Description: prints "Programming is like building a multilingual puzzle"
  *
- * Returns 0
+ * return: returns 0, success
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
