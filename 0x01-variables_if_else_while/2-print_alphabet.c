08a02db30f7e73237ba47ac785c314d5e1434796
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-03-11 17:57:47 +0100

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,19 +4,19 @@
  * main - Entry point
  *
  * Description: a program that prints alphabets in lowercase
- * 
+ *
  * Return: returns 0
  **/
 
 int main(void)
 {
-  char alpha;
-  alpha = 'a';
-  while (alpha <= 'z')
-  {
-  printf("%c ", alpha);
-  alpha++;
-  }
-  printf("\n");
-  return (0);
+char alpha;
+alpha = 'a';
+while (alpha <= 'z')
+{
+printf("%c", alpha);
+alpha++;
+}
+printf("\n");
+return (0);
 }
