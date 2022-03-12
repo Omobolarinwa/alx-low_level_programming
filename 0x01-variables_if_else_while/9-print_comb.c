3ea7c29306e6e46074e5449b84c8897b553c8552
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-03-12 05:03:46 +0100

    print combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints all possinle combinations
+ * of single-digit number
+ *
+ * Return: returns 0
+ **/
+
+int main(void)
+{
+int i, j;
+for(i = 0; i <= 9; i++)
+{
+for(j=0; j <= 9; j++)
+{
+printf("%d%d, ", i, j);
+}
+}
+}
