3257a996211941f588f9f572735d3e7f9b2c4638
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-03-12 05:24:45 +0100

    print combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,22 @@
 
 int main(void)
 {
-int i;
-for (i = 0; i <= 9; i++)
-{
-printf("%d, ", i);
-}
-putchar('\n');
-return (0);
+	int i;
+
+	i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
 }
