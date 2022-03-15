625d003e9fc86d344d86777f2881158a6621bf3d
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-03-15 14:55:21 +0100

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Description: A program that prints aphabets in small letter
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet();
+
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+
+void print_alphabet() {
+char alpha;
+
+for (alpha = 'a'; alpha <='z'; alpha++)
+{
+putchar(alpha);
+}
+putchar('\n');
+}
