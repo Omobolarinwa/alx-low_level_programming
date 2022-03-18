b27fc24b7785696cb653554942b66af04cfd40cb
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-03-18 05:07:22 +0100

    is_digit and mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - a function that multiplies two ints
+ *
+ * @a: First int
+ * @b: Second int
+ *
+ * Return: returns 0.
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
