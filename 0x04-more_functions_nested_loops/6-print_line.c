60708f28de2f821861bf75083d9e9ddf9749fed1
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-03-18 05:28:50 +0100

    mandatories

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - draws straight line in the terminal
+ *
+ * @n: Number of lines wanted
+ *
+ * Return: returns 0.
+ */
+
+void print_line(int n)
+{
+int i;
+
+if (n <= 0)
+_putchar('\n');
+else
+{
+for (i = 0; i < n; i++)
+_putchar('_');
+
+_putchar('\n');
+}
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal
+ *
+ * @n: Number of spaces wanted
+ *
+ * Return: returns 0.
+ */
+
+void print_diagonal(int n)
+{
+int i, j;
+
+if (n <= 0)
+_putchar('\n');
+else
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+_putchar(' ');
+
+_putchar('\\');
+_putchar('\n');
+}
+
+}
+}
