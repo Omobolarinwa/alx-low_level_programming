8a7fa3b247814db59141f9ccf35769f4f8e45ba2
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-03-23 01:54:16 +0100

    updated print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,27 +3,26 @@
 
 /**
  * print_array - prints n elements of an array
+ *
  * @a: array of integers
  * @n: number of items to print
+ *
  * Return: void
  */
 
 void print_array(int *a, int n)
 {
-int i, count;
-
-i = 0;
-count = 1;
-if (n < 0)
-n = 0;
-if (n > 0)
+int i;
+for (i = 0; i < n; i++)
 {
-while (a[i] != '\0' && count < n)
+if ( == 0)
 {
-printf("%d, ", a[i]);
-i++;
-count++;
+printf("%d", a[i]);
 }
-printf("%d\n", a[i]);
+else
+{
+printf(", %d", a[i]);
 }
+}
+printf("\n");
 }
