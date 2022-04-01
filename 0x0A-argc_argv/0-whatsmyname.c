66e9870a2faba41d4f0fe5938389c0e91f9497dd
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-04-01 20:10:04 +0100

    readme, main.h, task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the program name
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: (0)
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
