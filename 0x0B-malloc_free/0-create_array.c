ab2e817158281d60ad9545f82d90460f19e7a59b
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-04-05 13:42:20 +0100

    updated create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *arr;
 
-	arr = (char*) malloc(size * sizeof(char));
+	arr = (char *) malloc(size * sizeof(char));
 	if (arr == NULL)
 		return (NULL);
 
