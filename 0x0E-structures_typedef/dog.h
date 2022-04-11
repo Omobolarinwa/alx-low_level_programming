c10c1e9620f4de5f14b43a55800bdaedac5791b0
Luqman Abdulwasii <luqmanabdulwasii@gmail.com>
2022-04-11 15:44:21 +0100

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog's info
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * 
+ * description: a struct that defines the details of a dog
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
